9467fbea39cf823f3b72bc4cc7f96b6f3bab21a5
Martin Flodin <mflodin@gmail.com>
2017-12-22 00:54:56 +0100

    Day 20-1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { mazerunner } from "./19";
+// import { mazerunner } from "./19";
 import { read } from "./utils";
 
-read("inputs/19.input.txt").then(text => {
-  let run = mazerunner(text);
-  console.log("1: ", run);
+read("inputs/20.input.txt").then(text => {
+  // let run = mazerunner(text);
+  console.log("1: ", 157); // particle with smallest v and no a
 
   // console.log("2: ", sendCount1);
 });
