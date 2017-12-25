5b6b7a58b8dcc12352347b0d785bc8ee5a6796e7
Martin Flodin <mflodin@gmail.com>
2017-12-25 20:12:40 +0100

    Day 21-2

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ import { generateRuleMap, Fractal, countPixels } from "./21";
 
 read("inputs/21.input.txt").then(text => {
   const seed = ".#./..#/###";
+  let i = 0;
 
   const fractal = new Fractal({ seed, ruleMap: generateRuleMap(text) });
 
-  fractal.tick();
-  fractal.tick();
-  fractal.tick();
-  fractal.tick();
-  fractal.tick();
+  for (; i < 5; i++) {
+    fractal.tick();
+  }
 
   console.log("1: ", countPixels(fractal.state));
+
+  for (; i < 18; i++) {
+    fractal.tick();
+  }
+  console.log("2: ", countPixels(fractal.state));
 });
